aa1b4f852cb9baab5f2fe7e1d44393b72c386754
Sərkan Məmmədov <serkan.m@ndu.edu.az>
2026-03-05 10:54:05 +0400

    Təmizlik: Test faylları və müvəqqəti arxivlər silindi

--- teacher/test_db.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'config/database.php';
-$db = Database::getInstance();
-print_r($db->fetchAll('SELECT id, title, lesson_type, duration_minutes, recording_path FROM live_classes ORDER BY id DESC LIMIT 5'));
-print_r($db->fetchAll('SELECT * FROM archived_lessons ORDER BY id DESC LIMIT 5'));
